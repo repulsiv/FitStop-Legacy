d20a38b8d6fd1f9abb7fd85f6b1030330db6e8d7
Shaital Desai <shaital.desai@sbcglobal.net>
2018-08-05 22:41:09 -0700

    adds chart toggle view

--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -14,52 +14,65 @@ import './../css/style.css';
 
 // };
 // {
-// class History extends React.Component{
-//   constructor(props) {
-//     this.state = {
-//       classname: 
-//     };
-//     this.changeClass = this.changeClass.bind(this);
-//   }
+class History extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartView: 'week'
+    };
+    this.changeView = this.changeView.bind(this);
+    this.renderView = this.renderView.bind(this);
+  }
 
-//   changeClass() {
+  changeView(option) {
+    this.setState({
+      chartView: option
+    })   
+  }
+
+  renderView() {
+    var view = this.state.chartView;
+    if (view === 'week') {
+      return <FitBarChart className="" workoutHistory={this.props.workoutHistory} />
+    } else if (view === 'month') {
+      return <FitBarMonthChart className="" workoutHistory={this.props.workoutHistory} />
+    } else {
+      return <FitBarYearChart className="" workoutHistory={this.props.workoutHistory} />
+    }
+  }
+
+  render () {
+    return (
+      <div className="history">
+        <h1> History </h1>
+        {this.props.loggedIn && this.props.workoutHistory.map(indivWorkout => <PastWorkout date={indivWorkout.date} lengthOfWorkout={indivWorkout.lengthOfWorkout} key={indivWorkout._id}/>)}
+        {this.props.loggedIn && 
+          <div>
+           <button onClick={() => this.changeView('week')}>Current Week Fitness Trend</button>
+            <button onClick={() => this.changeView('month')}>Current Month Fitness Trend</button>
+            <button onClick={() => this.changeView('year')}>Current Year Fitness Trend</button>
+            {this.renderView()}
+          </div>
+        }
+        {!this.props.loggedIn && (<span className='historyNotLoggedIn'>You are not currently logged in. Please Log In or Sign Up to view Workout History.</span>)}
+      </div>)
     
-//   }
+  }
+}
 
-//   render () {
-//     return (
-//       <div className="history">
-//         <h1> History </h1>
-//         {props.loggedIn && props.workoutHistory.map(indivWorkout => <PastWorkout date={indivWorkout.date} lengthOfWorkout={indivWorkout.lengthOfWorkout} key={indivWorkout._id}/>)}
-//         {props.loggedIn && 
-//           <div>
-//             <button onClick={this.hangeClass}>Current Month Fitness Trend</button>
-//             <button onClick={this.changeClass}>Current Year Fitness Trend</button>
-//             <FitBarChart className="" workoutHistory={props.workoutHistory} />
-//             <FitBarMonthChart className="" workoutHistory={props.workoutHistory} />
-//             <FitBarYearChart className="" workoutHistory={props.workoutHistory} />
-//           </div>
-//         }
-//         {!props.loggedIn && (<span className='historyNotLoggedIn'>You are not currently logged in. Please Log In or Sign Up to view Workout History.</span>)}
-//       </div>)
-//     )
-//   }
-// }
+// var History = (props) => (
+//   <div className="history">
+//     <h1> History </h1>
+//     {console.log('HISTORY', props)}
+//     {props.loggedIn && props.workoutHistory.map(indivWorkout => <PastWorkout date={indivWorkout.date} lengthOfWorkout={indivWorkout.lengthOfWorkout} key={indivWorkout._id}/>)}
+//     {props.loggedIn && <div><button>Current Month Fitness Trend</button>
+//       <button>Current Year Fitness Trend</button></div>}
+//     {props.loggedIn && (<FitBarChart workoutHistory={props.workoutHistory} />)} 
+//     {props.loggedIn && (<FitBarMonthChart workoutHistory={props.workoutHistory} />)} 
+//     {props.loggedIn && (<FitBarYearChart workoutHistory={props.workoutHistory} />)}
+//     {!props.loggedIn && (<span className='historyNotLoggedIn'>You are not currently logged in. Please Log In or Sign Up to view Workout History.</span>)}
+//   </div>
 // );
-
-var History = (props) => (
-  <div className="history">
-    <h1> History </h1>
-    {console.log('HISTORY', props)}
-    {props.loggedIn && props.workoutHistory.map(indivWorkout => <PastWorkout date={indivWorkout.date} lengthOfWorkout={indivWorkout.lengthOfWorkout} key={indivWorkout._id}/>)}
-    {props.loggedIn && <div><button>Current Month Fitness Trend</button>
-      <button>Current Year Fitness Trend</button></div>}
-    {props.loggedIn && (<FitBarChart workoutHistory={props.workoutHistory} />)} 
-    {props.loggedIn && (<FitBarMonthChart workoutHistory={props.workoutHistory} />)} 
-    {props.loggedIn && (<FitBarYearChart workoutHistory={props.workoutHistory} />)}
-    {!props.loggedIn && (<span className='historyNotLoggedIn'>You are not currently logged in. Please Log In or Sign Up to view Workout History.</span>)}
-  </div>
-);
 
 
 export default History;
